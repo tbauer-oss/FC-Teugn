5118317cd9610cd4c2321bcfcacbf19f379fe953
tbauer-oss <tbauer.mail@googlemail.com>
2025-12-03 18:50:06 +0100

    Delete fc-teugn-backend directory

--- fc-teugn-backend/src/index.ts
+++
@@ -1,7 +0,0 @@
-import app from './server';
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`FC Teugn backend listening on http://localhost:${PORT}`);
-});
--- fc-teugn-backend/src/types/express.d.ts
+++
@@ -1,9 +0,0 @@
-import { AuthUser } from '../middleware/auth';
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: AuthUser;
-    }
-  }
-}
