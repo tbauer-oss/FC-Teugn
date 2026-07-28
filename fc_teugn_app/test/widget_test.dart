27c66b93999f461a8e2bd1e39b9fe4d2e0d436d2
tbauer-oss <tbauer.mail@googlemail.com>
2026-07-28 23:22:01 +0200

    test: mock secure storage in login widget test

--- fc_teugn_app/test/widget_test.dart
+++ fc_teugn_app/test/widget_test.dart
@@ -1,9 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fc_teugn_app/app.dart';
 
 void main() {
   testWidgets('shows the FC Teugn login', (tester) async {
+    FlutterSecureStorage.setMockInitialValues({});
+
     await tester.pumpWidget(const ProviderScope(child: FCTeugnApp()));
     await tester.pumpAndSettle();
 
